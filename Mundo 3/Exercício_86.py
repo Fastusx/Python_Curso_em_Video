04033c09d65c160036181d2cbd17d2dd28e992f3
Arthur dos Santos Lima <83731694+Fastusx@users.noreply.github.com>
2022-04-09 16:53:46 -0300

    Delete Exercício_86.py

--- Mundo 3/Exercício_86.py
+++
@@ -1,9 +0,0 @@
-matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-for f in range(0, 3):
-    for c in range(0, 3):
-        matriz[f][c] = int(input(f'Digite um número para [{f}, {c}]: '))
-print('-=' * 30)
-for f in range(0, 3):
-    for c in range(0, 3):
-        print(f'[{matriz[f][c]:^5}]', end='')
-    print()
