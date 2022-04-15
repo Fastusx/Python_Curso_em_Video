31c2f4aa694e49c24cccfa5b4439ad2351adcf80
Arthur dos Santos Lima <83731694+Fastusx@users.noreply.github.com>
2022-04-15 15:08:17 -0300

    Delete Exercício_77.py

--- Mundo 3/Exercício_77.py
+++
@@ -1,8 +0,0 @@
-palavras = ('Pumpum', 'Balacobaco', 'Arthur', 'Egoismo', 'Odio', 'Solidão', 'Duvidas', 'incerto', 'Injusto', 'insanidade')
-for termo in palavras:
-    print(f' Na palavra {termo.upper()} temos: ')
-    for letras in termo:
-        if letras.lower() in 'aeiou':
-            print(f'{letras}')
-print()
-
