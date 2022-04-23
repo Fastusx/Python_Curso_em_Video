df15c831921563347571590a56d7785d73bf6c09
Arthur dos Santos Lima <83731694+Fastusx@users.noreply.github.com>
2022-04-23 14:10:35 -0300

    Utiliza módulos do diretório 'modulos'

---
+++ Mundo 3/Exercício_107.py
@@ -0,0 +1,5 @@
+﻿from modulos import moeda
+num = float(input('Preço: R$'))
+print(f'A metade de {num} é {moeda.metade(num):.1f}')
+print(f'O dobro de {num} é {moeda.dobro(num):.1f}')
+print(f'Aumentando 10% de {num}, ficaria {moeda.aumento(num):.1f}')
---
+++ Mundo 3/Exercício_108.py
@@ -0,0 +1,8 @@
+﻿from modulos import moeda
+num = float(input('Preço: R$'))
+mais = int(input('Digite um valor de porcentagem para AUMENTAR: '))
+menos = int(input('Digite um valor de porcentagem para DIMINUIR: '))
+print(f'A metade de {moeda.moeda(num)} é {moeda.moeda(moeda.metade(num))}')
+print(f'O dobro de {moeda.moeda(num)} é {moeda.moeda(moeda.dobro(num))}')
+print(f'Aumentando {mais}% de {moeda.moeda(num)}, ficaria {moeda.moeda(moeda.aumento(num, mais))}')
+print(f'Diminuindo {menos}% de {moeda.moeda(num)}, ficaria {moeda.moeda(moeda.diminuir(num, menos))}')
