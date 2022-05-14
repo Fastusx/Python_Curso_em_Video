4dd06b12c34c21ca5f7adecc93109805b685cf10
Arthur dos Santos Lima <83731694+Fastusx@users.noreply.github.com>
2022-05-14 20:45:06 -0300

    Delete Exercício_112.py

--- Mundo 3/Exercício_112.py
+++
@@ -1,5 +0,0 @@
-from utilidadescev import moeda, dado
-num = dado.leiadinheiro('Digite o preço:R$ ')
-mais = int(input('Digite um valor de porcentagem para AUMENTAR: '))
-menos = int(input('Digite um valor de porcentagem para DIMINUIR: '))
-moeda.resumo(num, mais, menos)
