fa2378d28d02b4f79916573cc5c0e273a97aeb96
Arthur dos Santos Lima <83731694+Fastusx@users.noreply.github.com>
2022-05-14 20:46:32 -0300

    Importações feitas pela pasta 'utilidadescev'.

---
+++ Mundo 3/Exercício_109.py
@@ -0,0 +1,8 @@
+﻿from utilidadescev import moeda
+num = float(input('Preço: R$'))
+mais = int(input('Digite um valor de porcentagem para AUMENTAR: '))
+menos = int(input('Digite um valor de porcentagem para DIMINUIR: '))
+print(f'A metade de {moeda.moeda(num)} é {moeda.metade(num,True)}')
+print(f'O dobro de {moeda.moeda(num)} é {moeda.dobro(num,True)}')
+print(f'Aumentando {mais}% de {moeda.moeda(num)}, ficaria {moeda.aumento(num, mais, True)}')
+print(f'Diminuindo {menos}% de {moeda.moeda(num)}, ficaria {moeda.diminuir(num, menos, True)}')
---
+++ Mundo 3/Exercício_110.py
@@ -0,0 +1,5 @@
+﻿from utilidadescev import moeda
+num = float(input('Preço: R$'))
+mais = int(input('Digite um valor de porcentagem para AUMENTAR: '))
+menos = int(input('Digite um valor de porcentagem para DIMINUIR: '))
+moeda.resumo(num, mais, menos)
